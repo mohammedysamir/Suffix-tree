e3d2a3f54fd98b235aeff566aad29f91fbf2a1ed
Mohammed Yasser <mohammedre4a@gmail.com>
2020-12-31 03:32:42 +0200

    refactor : using char array and matrix instead of vectors<string>.

--- SuffixTree.cpp
+++ SuffixTree.cpp
@@ -4,29 +4,41 @@ using namespace std;
 class SuffixTree {
 	int Size;	//Size of string + sentinal
 	char* char_arr;	//Save character array
-	vector<string> Suffixes;
+	char** Suffixes;
 public:
 	SuffixTree(int s ,string a) {
 		Size = s;
-		char_arr =new char[Size+1];
-		for (int i = 0; i < Size; i++) {	//populate character array from string
+		char_arr =new char[Size];
+		for (int i = 0; i <Size; i++) {	//populate character array from string
 			char_arr[i] = a[i];
 		}
+		Suffixes=new char*[Size];		//build matrix of suffixes
+		for(int i=0;i<Size;i++)
+			Suffixes[i]=new char[Size];
 	}
 	void build_Suffix() {
 		string suffix;
-		for(int j=0;j<Size;j++){
+		int row_suffix=0;
+		for(int j=0;j<Size;j++ ,row_suffix++ ){
 			suffix = "";
 			for (int i = Size-j-1; i <Size; i++) {
 				suffix += char_arr[i];
 			}
-			Suffixes.push_back(suffix);		//pushes suffixes
+			for(int k=0, column_suffix=0;k<suffix.size();k++, column_suffix++){
+				Suffixes[row_suffix][column_suffix]=suffix[k];
+			}
 		}
 		
 	}
 	void printSuffixes() {
-		for (int i = 0; i < Suffixes.size(); i++) {
-			cout << Suffixes.at(i) << endl;
-		}
+		for (int i = 0; i <Size; i++) {	//for rows
+			int j = 0;
+			for(;j<Size;j++){	//for columns
+				if (Suffixes[i][j] != '$')
+					cout << Suffixes[i][j];
+				else break;
+			}
+			cout<<Suffixes[i][j]<<endl; //print sentinal
+		}	
 	}
 };
